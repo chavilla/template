bb843ffe4e6f2f0741863a4a4c3687a8576227dd
chavilla <jcharris.villa@gmail.com>
2020-09-29 22:51:14 -0500

    Se hace el menu para dispositivos móviles

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
-$(function(){
+"use strict";
 
-$('.count-down').countdown('2020/10/01 00:00:00',function(event){
-    $("#dias").html(event.strftime("%D"));
-    $("#horas").html(event.strftime("%H"));
-    $("#minutos").html(event.strftime("%M"));
-    $("#segundos").html(event.strftime("%S"));
-});
+window.addEventListener("load", () => {
+  //Cierra el menú en modo móvil
+  function toggleMenu() {
+    //Identifica el boton de salir y el menú
+    const btnShowMenu = document.querySelector("#btnShowMenu");
+    const btnExitMenu = document.querySelector("#exitMenu");
+    const menu = document.querySelector(".main-menu");
 
+    btnExitMenu.addEventListener("click", () => {
+     fadeOut(menu)
+    });
+
+    btnShowMenu.addEventListener("click", () => {
+     fadeIn(menu);
+    });
+  } // end toggleMenu
+
+  function fadeOut(el){
+    el.style.opacity = 1;
+  
+    (function fade() {
+      if ((el.style.opacity -= .1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+  
+  function fadeIn(el, display){
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+  
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+
+  toggleMenu();
 });
