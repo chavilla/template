a1a4c18172786a9e23035d375a06f8152b866ef7
chavilla <jcharris.villa@gmail.com>
2020-10-02 23:11:17 -0500

    Animacion

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 "use strict";
 
 window.addEventListener("load", () => {
-  //Cierra el menú en modo móvil
+
+  // Variables globales
+  const menu = document.querySelector(".main-menu");
+  const title = document.querySelector('#title');
+  const btnMain= document.querySelector('#btnMain');
+
   function toggleMenu() {
     //Identifica el boton de salir y el menú
     const btnShowMenu = document.querySelector("#btnShowMenu");
     const btnExitMenu = document.querySelector("#exitMenu");
-    const menu = document.querySelector(".main-menu");
+    
 
     btnExitMenu.addEventListener("click", () => {
      fadeOut(menu)
@@ -43,4 +48,49 @@ window.addEventListener("load", () => {
   };
 
   toggleMenu();
+
+
+  // Zona de funciones anónimas
+  window.onresize=function(e){
+    const widthOfWindow=window.innerWidth;
+    if (widthOfWindow>=768) {
+      fadeIn(menu);
+    }else{
+      menu.style.display='none';
+    }
+  }
+
+
+  //Oculta el título y muestra a los tres segundos
+ 
+
+  setTimeout(()=>{
+    btnMain.classList.remove('translate-right');
+  },1000);
+
+
+  //efecto máquina de escribir
+  function writing(str){
+    let arrFromString=str.split('');
+
+    let i=0;
+    let printString=setInterval(()=>{
+      title.innerHTML+=arrFromString[i];
+
+      i++;
+      if (i===arrFromString.length) {
+        clearInterval(printString);
+        
+        setTimeout(() => {
+          title.innerHTML='';
+        writing(str);
+        }, 3000);
+      }
+    },200);
+
+  }
+
+  writing('Innovación y Precios bajos Con Chaviweb');
+ 
+
 });
---
+++ js/typed.js
@@ -0,0 +1,20 @@
+"use strict";
+
+window.addEventListener("load", () => {
+
+    const typed=new Typed('.typed', {
+        strings:['CHAVIWEB'],
+        typeSpeed: 275,
+        startDelay:1000,
+        backSpeed:180,
+        smartBackspace:true,
+        shuffle:false,
+        backDelay:6500,
+        loop:true,
+        loopCount:false,
+        showCursor:true,
+        cursorChar: '|',
+        contenType: 'html'
+    });
+
+});
